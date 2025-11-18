c2667e76b219ed333143816ac42cc07493c44b86
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-18 16:31:05 -0500

    feat: add seed script for initial database population with admin user and sample posts

---
+++ backend/prisma/seed.js
@@ -0,0 +1,50 @@
+import { prisma } from './client.js';
+import bcrypt from 'bcrypt';
+
+async function main() {
+  const hashedPassword = await bcrypt.hash('123456', 10);
+
+  const author = await prisma.user.upsert({
+    where: { username: 'admin' },
+    update: {},
+    create: {
+      username: 'admin',
+      password: hashedPassword,
+      posts: {
+        create: [
+          {
+            title: 'Hypertrophy vs Strength: Which to Choose?',
+            content:
+              'For aesthetics, the range of 8-12 repetitions is usually ideal...',
+            published: true,
+            comments: {
+              create: [
+                {
+                  content: 'Great article! It helped me a lot.',
+                  username: 'GymNewbie2025',
+                },
+              ],
+            },
+          },
+          {
+            title: 'The Truth About Creatine',
+            content:
+              'It is the supplement with the most scientific evidence...',
+            published: false,
+          },
+        ],
+      },
+    },
+  });
+
+  console.log('Seed executed successfully 🌱');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
